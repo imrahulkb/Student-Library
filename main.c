53c724a19a9f57f712306a352d71195e9356bbc3
imrahulkb <90493221+imrahulkb@users.noreply.github.com>
2021-09-16 19:46:25 +0530

    Update main.c
    only 3 functions have been defined yet in this program. #secondedit

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main()
 {
   int cho1;
   do{
-    printf("\n 1. Add Records ");
+     printf("\n 1. Add Records ");
      printf("\n2. Show Records");
      printf("\n3. List records ");
      printf("\n4. Search Records ");
@@ -56,5 +56,66 @@ int main()
     }
   }while(cho1>=1 && cho1<=7);
   getch();
+}       
+int add_reco()      //1st_funtion_other_than_main
+{
+  struct student book x;
+  int a;
+  char cho1;
+  FILE *xyz;
+  xyz=fopen("dkte1.txt","ab+");
+  do
+  {
+    /* int ibsn,price,edition;
+  char bnam[20],auth[30];*/
+       printf("\n Enter Book's Name:");
+   scanf("%d",&x.bnam);
+   printf("\n Enter Name of author:");
+   scanf("%d",&x.auth);
+   printf("\n Enter Edition:");
+   scanf("%d",&x.edition);    
+   printf("\n Enter IBSN:");
+   scanf("%d",&x.ibsn);
+   printf("\n Enter Price:");
+   scanf("%d",&x.price);
+   printf("Do you want to Continue?\n Enter Y or N");
+    cho1=getche();
+   }while( cho1 =='Y' || cho1=='y')
+    fclose(xyz);
 }
 
+int show_reco()  //2nd function
+{
+  struct student book x;
+  int a;
+  char cho1;
+  FILE *xyz;
+  xyz=fopen("dkte1.txt","ab+");
+  do
+  {
+   
+   printf("\n Enter Book's Name: %c",x.bnam);
+   printf("\n Enter Name of author: %c",x.auth);
+   printf("\n Enter Edition: %d",x.edition);
+   printf("\n Enter IBSN: %d",x.ibsn);
+   printf("\n Enter Price: %d",x.price);
+   printf("Do you want to Continue?\n Enter Y or N");
+    cho1=getche();
+   }while( cho1 =='Y' || cho1=='y')
+    fclose(xyz);
+}
+
+int file_info()
+{
+  int file_size, tot_reco;
+  struct book x;
+  FILE *xyz;
+  xyz=fopen("dkte1.txt","ab+");
+  printf("\n FILE INFO");
+  fseek(xyz,o,SEEK_END);
+  fsize=ftell(xyz);
+  printf("\n File size=%d",filesize);
+  totreco=filesize/sizeof(x);
+  printf("Total No. of recoed = %d",totreco);
+}
+    
